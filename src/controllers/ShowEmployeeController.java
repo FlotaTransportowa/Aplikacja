b7cdfddc81c8b23b34c78842616cee6f771cb619
danielHeap <30367211+danielHeap@users.noreply.github.com>
2018-01-07 22:43:09 +0100

    update Daniel

--- src/controllers/ShowEmployeeController.java
+++ src/controllers/ShowEmployeeController.java
@@ -1,5 +1,35 @@
 package controllers;
 
+import database.Employee;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+import static database.Employee.findAllEmployees;
+import static sample.Main.returnEmployee;
+
+
 public class ShowEmployeeController extends Controller {
+
+    @FXML private TableView<Employee> employeeTable;
+    private  ObservableList<Employee> data;
+    @FXML void initialize() throws Exception {
+
+        data=FXCollections.observableArrayList(findAllEmployees());
+        employeeTable.setItems(data);
+        employeeTable.setEditable(true);
+    }
+
+    @FXML
+    void saveData(){
+
+        data = employeeTable.getItems();
+        for (Employee e: data
+             ) {
+            System.out.println(e.getId()+" "+e.getLastName());
+        }
+        System.out.println("Zapisano");
+    }
 
 }
