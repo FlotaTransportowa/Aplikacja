d9ab4533fd071733889e4bb9f3624ef78a6112b2
Maks <mrmaksik95@gmail.com>
2018-01-27 22:07:52 +0100

    MJ - wyswietlanie maszyn

--- src/controllers/LoggedController.java
+++ src/controllers/LoggedController.java
@@ -159,8 +159,11 @@ public class LoggedController extends Controller {
 
             ShowMachineController showMachineController = loader.getController();
             showMachineControllerList.add(showMachineController);
+            showMachineController.start();
             showMachineController.setLoggedController(this);
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/controllers/ShowMachineController.java
+++ src/controllers/ShowMachineController.java
@@ -1,72 +1,79 @@
 package controllers;
 
 import database.Machine;
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import manager.GlobalManager;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import models.MachineModel;
+import org.controlsfx.control.table.TableRowExpanderColumn;
 
-import javax.persistence.EntityManager;
-import java.io.IOException;
 
-public class ShowMachineController {
+public class ShowMachineController extends Controller{
     private LoggedController loggedController;
 
-    @FXML private TableView<Machine> machineTable;
+    @FXML private TableView<Machine> table;
     private static ObservableList<Machine> data;
     private MachineModel machineModel = new MachineModel();
 
-    @FXML void initialize() throws Exception {
-        data = FXCollections.observableArrayList(machineModel.getAll());
-        machineTable.setItems(data);
+    public void start() throws Exception{
+        StackPane root = new StackPane();
 
+        table = new TableView<>();
+        //table.setPrefWidth(500);
 
-        for (
-                Machine e : data
-                ) {
-            ButtonBar buttonBar = e.getButtonBar();
-            if (buttonBar.getButtons().size() > 0)
-                continue;
-            Button editButton = new Button("Edytuj");
-            editButton.setOnAction(ev -> {
-                        if (e == null)
-                            System.out.println(e.getId());
-                        else
-                            try {
-                                loggedController.editMachine(e);
-                            } catch (IOException e1) {
-                                e1.printStackTrace();
-                            }
-                    }
-            );
-            Button deleteButton = new Button("Usuń");
-            deleteButton.setOnAction(ev -> {
-                        EntityManager entityManager = GlobalManager.getManager();
-                        entityManager.getTransaction().begin();
-                        try {
-                            entityManager.remove(e);
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        } finally {
-                            entityManager.getTransaction().commit();
-                        }
-                        System.out.println("Usunięto " + e.getId());
-                        try {
-                            initialize();
-                        } catch (Exception e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-            );
+        TableRowExpanderColumn<Machine> expander = new TableRowExpanderColumn<>(this::createEditor);
 
-            ButtonBar.setButtonData(editButton, ButtonBar.ButtonData.BIG_GAP);
-            ButtonBar.setButtonData(deleteButton, ButtonBar.ButtonData.BIG_GAP);
-            buttonBar.getButtons().addAll(editButton, deleteButton);
-        }
+        TableColumn<Machine, String> vin = new TableColumn<>("VIN");
+        vin.setCellValueFactory(new PropertyValueFactory<>("VIN"));
+
+        TableColumn<Machine, String> nrRej = new TableColumn<>("Numer rejestracyjnyyyyy");
+        nrRej.setCellValueFactory(new PropertyValueFactory<>("registrationNumber"));
+
+        table.getColumns().addAll(expander, vin, nrRej);
+        table.setItems(initialize());
+
+        root.getChildren().add(table);
+    }
+
+    private GridPane createEditor(TableRowExpanderColumn.TableRowDataFeatures<Machine> param){
+        GridPane editor = new GridPane();
+        editor.setPadding(new Insets(10));
+        editor.setHgap(10);
+        editor.setVgap(5);
+
+        Machine machine = param.getValue();
+        TextField vinText = new TextField(machine.getVIN());
+        TextField nrRejText = new TextField(machine.getRegistrationNumber());
+
+        Button save = new Button("Zapisz");
+        save.setOnAction(e -> {
+            machine.setVIN(vinText.getText());
+            machine.setRegistrationNumber(nrRejText.getText());
+            param.toggleExpanded();
+        });
+
+        Button cancel = new Button("Anuluj");
+        cancel.setOnAction(e1 -> param.toggleExpanded());
+
+        editor.addRow(2, save, cancel);
+
+        return editor;
+    }
+
+    @FXML
+    public ObservableList<Machine> initialize() throws Exception {
+        return FXCollections.observableArrayList(machineModel.getAll());
     }
 
     public void setLoggedController(LoggedController loggedController){
